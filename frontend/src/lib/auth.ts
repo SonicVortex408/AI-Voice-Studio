f64799afd707238bfa153635b91f886a68aec9d3
Riya <riyapataskar@example.com>
2026-04-21 16:25:32 +0530

    Finallys commit

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -20,7 +20,7 @@ const prisma = new PrismaClient();
 export const auth = betterAuth({
   trustedOrigins: [
     "http://localhost:3000",
-    "https://compiler-subject-scotland-worcester.trycloudflare.com"
+    "https://ai-voice-studio-project.vercel.app/"
   ],
 
   database: prismaAdapter(prisma, {
